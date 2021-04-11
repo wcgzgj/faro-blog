ba33a3804b58c069167177a9d5f5ba04012e829b
wcgzgj <zhangjia_good@126.com>
2021-04-12 00:54:37 +0800

    完成文章管理的页面搭建

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
+import AdminDocEdit from '../views/admin/admin-doc-edit.vue'
 
 
 const routes: Array<RouteRecordRaw> = [
@@ -19,6 +20,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/admin/doc',
     name: 'AdminDoc',
     component: AdminDoc
+  },
+  {
+    path: '/admin/docEdit',
+    name: 'AdminDocEdit',
+    component: AdminDocEdit
   }
 ]
 
---
+++ web/src/views/admin/admin-doc-edit.vue
@@ -0,0 +1,93 @@
+<template>
+
+    <el-container class="docEditPlace" direction="vertical">
+        <el-row>
+            <el-col :span="20" :offset="2">
+                <el-input placeholder="请输入标题" v-model="input1">
+                    <template #prepend>Title:</template>
+                </el-input>
+            </el-col>
+        </el-row>
+
+        <el-row>
+            <el-col :span="20" :offset="2">
+                <el-input
+                        placeholder="请输入文章描述"
+                        type="textarea"
+                        :autosize="{ minRows: 2, maxRows: 4}"
+                        v-model="input1">
+                </el-input>
+            </el-col>
+        </el-row>
+
+        <el-row>
+            <el-col :span="20" :offset="2">
+                <el-button type="primary" icon="el-icon-view">预览</el-button>
+
+                <el-button type="primary" icon="el-icon-check">保存</el-button>
+            </el-col>
+        </el-row>
+
+        <el-row>
+            <el-col :span="20" :offset="2">
+                <div id="content"></div>
+            </el-col>
+        </el-row>
+
+    </el-container>
+
+
+</template>
+
+<script>
+    // 管理员文档编辑界面
+
+    import E from 'wangeditor';
+    import { onMounted,ref } from 'vue';
+    import axios from "axios";
+    import {ElMessage} from "element-plus";
+    import { ElMessageBox } from 'element-plus';
+
+    export default {
+        name: "admin-doc-edit",
+
+        setup() {
+            /**
+             * ------变量区------
+             */
+            const editor = new E('#content');
+
+            const doc = ref();
+            doc.value={};
+
+            const content = ref();
+            content.value = {};
+
+
+            /**
+             * ------函数区------
+             */
+
+            onMounted(() => {
+                editor.create();
+            });
+
+
+            /**
+             * ------返回------
+             */
+            return {
+
+            }
+        }
+    }
+
+</script>
+
+
+<style scoped>
+    .docEditPlace .el-row{
+        margin-top: 20px;
+    }
+
+</style>
