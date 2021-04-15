d9d0c8fe02d6b28c6deb16b077bbe53b3168f30e
wcgzgj <zhangjia_good@126.com>
2021-04-15 08:46:38 +0800

    背景图片

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -28,7 +28,7 @@
                 用户管理
             </el-link>
         </el-menu-item>
-        <el-menu-item index="/admin/status">
+        <el-menu-item index="/report">
             <el-link :underline="false" icon="el-icon-s-marketing">
                 报表统计
             </el-link>
@@ -38,25 +38,134 @@
                 关于我
             </el-link>
         </el-menu-item>
+
+        <div style="margin-right: 40px">
+            <!--<el-link icon="el-icon-edit" class="login-menu" v-show="user.id" style="color: red">-->
+            <!--    <span>你好: {{user.name}}</span>-->
+            <!--</el-link>-->
+            <!--&nbsp;&nbsp;-->
+            <!--<el-link class="login-menu" @click="showLoginModal" v-show="!user.id">-->
+            <!--    <span>登录</span>-->
+            <!--</el-link>-->
+            <a class="login-menu" v-show="user.id">
+                <span>你好: {{user.name}}</span>
+            </a>
+
+            <a class="login-menu" @click="showLoginModal" v-show="!user.id">
+                <span>登录</span>
+            </a>
+        </div>
+
     </el-menu>
+
+
+
+
+
 </template>
 
 <script>
-    import { defineComponent, onMounted, ref, createVNode } from 'vue';
+    import { defineComponent, onMounted, ref, createVNode ,computed} from 'vue';
     import axios from 'axios';
+    import store from '@/store'
+    import { ElMessage } from 'element-plus'
     export default {
         name: "the-header",
 
 
-
-
         setup() {
+            const user = computed(() => store.state.user);
+
+            const loginUser = ref();
+            loginUser.value= {
+                loginName: "test",
+                password: "abc123"
+            };
+
+
+            const loginModalVisible = ref();
+            loginModalVisible.value=false;
+
+            const loginModalLoading = ref();
+            loginModalLoading.value=false;
+
+
+
+            /**
+             * ------方法区------
+             */
+            const showLoginModal= ()=> {
+                loginModalVisible.value=true;
+            }
+
+
+            const login = () => {
+                console.log("开始登录!")
+                loginModalLoading.value=true;
+
+                loginUser.value.password = hexMd5(loginUser.value.password + KEY);
+
+                axios.post("/user/login",loginUser.value).then((response)=>{
+                    loginModalLoading.value=false;
+                    const data = response.data;
+                    if (data.success) {
+                        loginModalVisible.value=false;
+                        ElMessage.success("登录成功!")
+                        /**
+                         * setUser: vuex 中 mutations中的方法
+                         *
+                         * 后面的参数，都是我们在 mutations 中自定义方法的参数
+                         * state 参数因为是自带的，所以没有必要写
+                         */
+                        store.commit("setUser",data.content);
+                    } else {
+                        /**
+                         * 使用 antd 的组件，弹出错误信息
+                         */
+                        ElMessage.error(data.message);
+                    }
+                });
+            }
+
+            /**
+             * 退出登录
+             */
+            const logout = () => {
+                console.log("退出登录!")
+
+                axios.get("/user/logout/"+user.value.token).then((response)=>{
+                    const data = response.data;
+                    if (data.success) {
+                        loginModalVisible.value=false;
+                        ElMessage.success("退出登录成功!")
+
+                        /**
+                         * 退出登录时
+                         * 将 sessionStorage 中对应 user的信息清空
+                         * 因为
+                         */
+                        store.commit("setUser", {});
+                    } else {
+                        /**
+                         * 使用 antd 的组件，弹出错误信息
+                         */
+                        ElMessage.error(data.message);
+                    }
+                });
+            }
 
             onMounted(()=> {
 
             });
 
             return {
+                user,
+                loginUser,
+                loginModalVisible,
+                loginModalLoading,
+
+
+                showLoginModal
 
             }
         }
@@ -64,5 +173,10 @@
 </script>
 
 <style scoped>
+    .login-menu {
+        float: right;
+        color: white;
+        padding-left: 10px;
+    }
 
 </style>
